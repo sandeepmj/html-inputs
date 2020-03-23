f115f0733152acca3e46b2b42c65546f457886a4
sandeepmj <sjnews@gmail.com>
2020-03-23 18:34:23 -0400

    added grab data for all form except range

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
+//grab items that thare checked
+$("input[type='checkbox']").on("click", function (e) {
+	//NO PREVENT DEFAULT. YOU WANT THE BOX TO CHECK
+	// $("ul li.active").removeClass("active"); //disable all active classes
+
+	//making checked items active
+	$("input[type='checkbox']:checked").each(function () {
+		var checkbox = $(this),
+			elective = checkbox.data("price");
+		console.log(elective)
+	});
+});
+
+// get radio button selections
 $("body").on("click", "#radio-btn", function (e) { // we must wait for the DOM to be ready as the browser can take a little while to read all of the HTML and build the elements
-	// 	// $("input[max], input[min]").on("keyup", function (e) { //any input that has a min or max defined
-	// 	// 	let ele = $(this), //the input field we're typing in
-	// 	// 		min = ele.attr("min"), //grab the minimum value as defined
-	// 	// 		max = ele.attr("max"), //grab the maximum value as defined
-	// 	// 		type = ele.attr("type"), //grab the type of input this is
-	// 	// 		value = ele.val(), //grab the value as entered
-	// 	// 		container = ele.closest("div");
 	output =
 		$('input[name=income]:checked',
 			'#tablet-radio').val();
 	console.log(output)
 });
-// data list capture
+
+// get dropdown menu selections
+$("body").on("click", "#submit", function (e) { // we must wait for the DOM to be ready as the browser can take a little while to read all of the HTML and build the elements
+	console.log($("#tablet-pulldown").val());
+});
+
+// // get datalist selections
 $("input[name=fav-sports]").bind('input', function () {
-
 	console.log($(this).val());
-
-
 });
